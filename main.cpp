b2bc96b1e65039b87cc9f60ba82abf413190be16
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-08-30 14:53:15 -0500

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include <isotream>
+#include <iostream>
 int main()
 {
 	std::cout<<"Bye world!!"<<"\n";
 	std::cout<<"This is further update to the code"<<"\n";
+	std::cout<<"final code ...says bye world"<<"\n";
 	return(0);
 }
